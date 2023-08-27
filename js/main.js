7cb59dbbeba3c50b1341506668cfca14a4657092
hmssarkar <hmssarkar@yahoo.com>
2023-08-27 21:08:41 +0530

    portfolio first step

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// function On(){
+//   document.getElementById("overlay").style.display= "block";
+//   document.getElementById("pop-up").style.display= "block";
+// }
+// function of(){
+//   document.getElementById("overlay").style.display= "none";
+//   document.getElementById("pop-up").style.display= "none";
+// }
+
+
+
+
+function bgChang(){
+  document.body.classList.toggle("dark-theme")
+}
+
+
+
+
