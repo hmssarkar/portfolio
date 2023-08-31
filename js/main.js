a0690554540ca62f6676315c5c82ed4a6d8e152f
hmssarkar <hmssarkar@yahoo.com>
2023-08-31 20:32:22 +0530

    update 2nd times

--- js/main.js
+++ js/main.js
@@ -16,4 +16,31 @@ function bgChang(){
 
 
 
+// js for toggle icon nav
+function icon(){
+  let div = document.getElementById("nav");
+  if(div.style.display !=='none'){
+    div.style.display ='none';
+  }else{
+    div.style.display='block';
+  }
+}
 
+function cross(){
+  let div = document.getElementById("nav");
+    div.style.display ='none';
+}
+
+
+// $(document).ready(function() {
+//   function checkWidth() {
+//       var windowSize = $(window).width();
+
+//       if (windowSize >= 479) {
+//         function cross(){
+//           let div = document.getElementById("nav");
+//             div.style.display ='none';
+//         }
+//       }
+//   }
+// })
